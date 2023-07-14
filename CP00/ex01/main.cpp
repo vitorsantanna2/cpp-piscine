fa9ea119db392b67df4cbe87c248d27a69405645
jsantann <jsantann@student.42.rio>
2023-07-14 17:10:30 -0300

    remove binarie

--- CP00/ex01/main.cpp
+++ CP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jsantann <jsantann@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 16:34:14 by jsantann          #+#    #+#             */
-/*   Updated: 2023/07/13 16:34:18 by jsantann         ###   ########.fr       */
+/*   Updated: 2023/07/13 17:01:44 by jsantann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,15 @@ int	main(void)
 {
 	std::string	input;
 	PhoneBook	book;
+
+	input = "text";
 	std::cout << "Welcome to the Santanna Books!" << std::endl;
 	std::cout << "The best phonebook of Earth!" << std::endl;
+	std::cout << "Please, type 'ADD' to add new contacts" << std::endl;
+	std::cout << "Type 'SEARCH' to search anyone contact" << std::endl;
+	std::cout << "Type 'EXIT' to exit the program" << std::endl;
+	while (input.compare("EXIT"))
+{
 	std::getline(std::cin, input);
+}
 }
