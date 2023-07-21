1024de4774eaa56b1d805c0945877402ce00acf7
jsantann <jsantann@student.42.rio>
2023-07-21 16:06:18 -0300

    Add getter and setters

--- CP00/ex01/Contact.cpp
+++ CP00/ex01/Contact.cpp
@@ -5,7 +5,7 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jsantann <jsantann@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/13 16:34:03 by jsantann          #+#    #+#             */
+/*	std::cout << "Destructor Called" << std::endl;   Created: 2023/07/13 16:34:03 by jsantann          #+#    #+#             */
 /*   Updated: 2023/07/13 16:34:07 by jsantann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -21,4 +21,59 @@ Contact::Contact()
 Contact::~Contact()
 {
 	return	;
+}
+
+std::string Contact::get_first_name(void)
+{
+	return (this->_first_name);
+}
+
+std::string Contact::get_last_name(void)
+{
+	return (this->_last_name);
+}
+
+std::string Contact::get_nickname(void)
+{
+	return (this->_nickname);
+}
+
+std::string Contact::get_phone(void)
+{
+	return (this->_phone);
+}
+
+std::string Contact::get_email(void)
+{
+	return (this->_email);
+}
+
+void	Contact::set_first_name(std::string first_name)
+{
+	this->_first_name = first_name;
+	return;
+}
+
+void	Contact::set_last_name(std::string last_name)
+{
+	this->_last_name = last_name;
+	return;
+}
+
+void	Contact::set_nickname(std::string nickname)
+{
+	this->_nickname = nickname;
+	return;
+}
+
+void	Contact::set_phone(std::string phone)
+{
+	this->_phone = phone;
+	return;
+}
+
+void	Contact::set_email(std::string email)
+{
+	this->_email = email;
+	return;
 }
