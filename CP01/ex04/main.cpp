c887ee22f6d27b1ccc327c2788843a6a8b58008b
jsantann <vitorpython@gmail.com>
2023-08-04 18:37:12 -0300

    remove files

--- CP01/ex04/main.cpp
+++ CP01/ex04/main.cpp
@@ -1,32 +1,70 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <sstream>
+
+void	error()
+{
+	std::cerr << "Error" << std::endl;
+	exit(0);
+}
+
+std::string copyfile(char **argv)
+{
+	std::string	line;
+	std::string	temp;
+	std::ifstream file(argv[1]);
+
+	if (file.is_open())
+	{
+		while (getline(file, temp))
+		{
+			line.append(temp);
+			line.append("\n");
+		}
+		file.close();
+	}
+	return (line);
+}
+
+std::string	change_text(std::string text, char **argv)
+{
+	std::string	temp;
+	std::string	line;
+	std::string	newvalue;
+	std::string	result;
+	std::istringstream iss(text);
+
+	while(std::getline(iss, line))
+	{
+		while (iss >> temp) {
+			if (!temp.compare(argv[2]))
+			{
+				newvalue.append(argv[3]);
+				result.append(newvalue);
+			}
+			else
+				result.append(temp);
+		}
+		result.append("\n");
+	}
+	return (result);
+}
+void	create_file(char **argv, std::string text)
+{
+	std::string  replace_file;
+	replace_file = argv[1];
+	replace_file.append(".replace");
+	std::ofstream new_file(replace_file);
+	new_file << text;   
+}
 
 int main(int argc, char **argv)
 {
-    std::string  line;
-    std::string  temp;
-    std::string  replace_file;
     if (argc != 4)
-    {
-        std::cout << "Error" << std::endl;
-        return (0);
-    }
-    std::ifstream file(argv[1]);
-    if (file.is_open())
-    {
-        replace_file = argv[1];
-        replace_file.append(".replace");
-        std::ofstream new_file(replace_file);
-        while(getline(file, temp))
-        {
-            line.append(temp);
-            line.append("\n");
-        }
-        file.close();
-        new_file << line;
-    }
-    else
-        std::cout << "File cant opened!" << std::endl;
+	    error();
+    if (copyfile(argv).empty()) 
+	    error();
+    create_file(argv, change_text(copyfile(argv), argv));
     return (0);
 }
